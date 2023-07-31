127b122a9e8f943e73161af7ec901f407884cea9
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-07-31 22:42:20 +0300

    user repository test

--- src/tests/repository/user.repository.test.ts
+++ src/tests/repository/user.repository.test.ts
@@ -91,3 +91,70 @@ describe('getUserIdDB', () => {
         expect(result[0].pwd).toBe('123');
     })
 })
+
+describe('createUserDB', () => {
+    test('1', async () => {
+        client.query.mockResolvedValue({
+            rows: [
+                {
+                    id: '1',
+                    name: 'TestName',
+                    surname: 'TestSurname',
+                    email: 'test@gmail.com',
+                    pwd: '123'
+                }
+            ]
+        })
+        const result = await createUserDB('TestName', 'TestSurname', 'test@gmail.com', '123');
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([
+            {
+                id: '1',
+                name: 'TestName',
+                surname: 'TestSurname',
+                email: 'test@gmail.com',
+                pwd: '123'
+            }
+        ])
+        expect(result.length).toBe(1);
+        expect(result[0].id).toBe('1');
+        expect(result[0].name).toBe('TestName');
+        expect(result[0].surname).toBe('TestSurname');
+        expect(result[0].email).toBe('test@gmail.com');
+        expect(result[0].pwd).toBe('123');
+    })
+})
+
+describe('upDataUserDB', () => {
+    test('1', async () => {
+        client.query.mockResolvedValue({
+            rows: [
+                {
+                    id: '1',
+                    name: 'TestName',
+                    surname: 'TestSurname',
+                    email: 'test@gmail.com',
+                    pwd: '123'
+                }
+            ]
+        })
+        const result = await upDataUserDB('1', 'TestName', 'TestSurname', 'test@gmail.com', '123');
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([
+            {
+                id: '1',
+                name: 'TestName',
+                surname: 'TestSurname',
+                email: 'test@gmail.com',
+                pwd: '123'
+            }
+        ])
+        expect(result.length).toBe(1);
+        expect(result[0].id).toBe('1');
+        expect(result[0].name).toBe('TestName');
+        expect(result[0].surname).toBe('TestSurname');
+        expect(result[0].email).toBe('test@gmail.com');
+        expect(result[0].pwd).toBe('123');
+    })
+})
+
