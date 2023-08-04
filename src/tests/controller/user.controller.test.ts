be600276855d911f56e5965539674f818563252a
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-08-04 17:38:53 +0300

    controller test

--- src/tests/controller/user.controller.test.ts
+++ src/tests/controller/user.controller.test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest';
 import app from '../../app';
 
-let id;
+let id
 
 test('POST:', async () => {
-    const res = await request(app).post(`/course`).send({ course: 'integrtest' });
+    const res = await request(app).post(`/user`).send({ name: 'Olga', surname: 'Grinko', email: '1234nnn@mail.ru', pwd: '1234' });
     id = res.body[0].id;
 
     expect(res.status).toBe(200);
@@ -12,28 +12,28 @@ test('POST:', async () => {
 })
 
 test('GET:', async () => {
-    const res = await request(app).get(`/course`);
+    const res = await request(app).get('/user');
 
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
 })
 
 test('GETById:', async () => {
-    const res = await request(app).get(`/course/${id}`);
+    const res = await request(app).get(`/user/${id}`);
 
     expect(res.status).toBe(200);
-    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body.length).toBe(1);
 })
 
 test('PUT:', async () => {
-    const res = await request(app).put(`/course/${id}`).send({ course: 'integrtest' });
+    const res = await request(app).put(`/user/${id}`).send({ name: 'Olga', surname: 'Grinko', email: '1234nnn@mail.ru', pwd: '1234' });
 
     expect(res.status).toBe(200);
     expect(res.body.length).toBe(1);
 })
 
 test('DELETE:', async () => {
-    const res = await request(app).delete(`/course/${id}`);
+    const res = await request(app).delete(`/user/${id}`);
 
     expect(res.status).toBe(200);
     expect(res.body.length).toBe(1);
