ae566a63c8829c2e8b5f4ac88af4c231d61dd7e5
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-08-07 14:21:21 +0300

    preview html

--- client/src/companents/Preview/Preview.js
+++ client/src/companents/Preview/Preview.js
@@ -4,11 +4,16 @@ function Preview() {
     return (
         <div className={style.preview}>
             <div className={style.previewInfo}>
-                <p>E-COURSE PLATFORM</p>
-                <h1>Learning and
-                    teaching online,
-                    made easy.</h1>
+                <p className={style.previewAbout}>E-COURSE PLATFORM</p>
+                <h1>Learning and teaching online, made easy.</h1>
+                <p className={style.textInfo}>Any subject, in any language, on any device, for all ages!</p>
+                <div className={style.previewBtn}>About platform</div>
 
+                <div className={style.previewStudentsNumber}>
+                    <div className={style.logImg}></div>
+                    <div className={style.numberPlus}></div>
+                </div>
+                <p className={style.previewStudents}>Students</p>
             </div>
             <div className={style.previewImg}></div>
         </div>
